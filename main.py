1d7b9742f761023d698550cc8cd92c2d65dab1ea
AnotherBot6 <107741072+AnotherBot6@users.noreply.github.com>
2022-11-14 17:32:39 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    #escribe tu código abajo de esta línea
+    print('Dame el número de mensajes')
+    msj=input()
+    print('Dame el número de megas')
+    megas=input()
+    print('Dame el número de minutos')
+    min=input()
+    msj_int=int(msj)
+    megas_flo=float(megas)
+    min_int=int(min)
+    
+    cobro=float(0.80)
+    pago=(cobro*msj_int)+(cobro*megas_flo)+(cobro*min_int)
+    print('El costo mensual es: ' ,str(float(pago)))
+
+if __name__ == '__main__':
+    main()
